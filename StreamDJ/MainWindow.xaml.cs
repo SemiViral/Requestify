606a5af68b262341441d4a441da5083b0fc92849
Travis Hill <aquatikjustice@gmail.com>
2016-02-29 14:15:32 -0600

    Initial Commit

---
+++ StreamDJ/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Requestify.Resources.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Requestify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<YoutubeResponse.Snippet> playlistItems = new List<YoutubeResponse.Snippet>();
+        public static Dictionary<string, string> playlist = new Dictionary<string, string>();
+
+        private void btnJoinChannel_Click(object sender, RoutedEventArgs e)
+        {
+            chatWindow.Source = new Uri($"http://www.twitch.tv/{txtbox_channel.Text}/chat");
+        }
+
+        private void btnGetVideos_Click(object sender, RoutedEventArgs e)
+        {
+            playlistItems = Youtube.GetPlaylistItems(txtbox_playlistID.Text);
+
+            playlistQueue.ItemsSource = Youtube.CreatePlaylist(playlistItems);
+        }
+    }
+}
---
+++ StreamDJ/Youtube.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using Requestify.Properties;
+using Requestify.Resources.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requestify
+{
+    class Youtube
+    {
+        public static List<YoutubeResponse.Snippet> GetPlaylistItems(string playlistID)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri($"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={playlistID}&key={Settings.Default.apiKey}");
+            var request = new RestRequest(Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            var results = JsonConvert.DeserializeObject<List<YoutubeResponse.Snippet>>(response.Content);
+            return results;
+        }
+
+        public static Dictionary<string, string> CreatePlaylist(List<YoutubeResponse.Snippet> playlistItems)
+        {
+            Dictionary<string, string> playlist = new Dictionary<string, string>();
+
+            foreach (var item in playlistItems)
+            {
+                playlist.Add(item.title, item.resourceId.videoId);
+            }
+
+            return playlist;
+        }
+    }
+}
